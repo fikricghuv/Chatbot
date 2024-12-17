a0ef463441309b9933d59f332a4451a6df6941f1
M Fikri Assegaf <72004446+fikricghuv@users.noreply.github.com>
2024-12-17 22:43:48 +0700

    Update settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,30 +1,18 @@
-# import os
+from pydantic_settings import BaseSettings
 # from dotenv import load_dotenv
 
-# load_dotenv()
-
-# class Settings:
-#     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-#     LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
-#     LANGCHAIN_PROJECT = os.getenv("LANGCHAIN_PROJECT")
-#     LANGCHAIN_TRACING_V2 = "true"
-
-# settings = Settings()
-
-from pydantic import BaseSettings
-from dotenv import load_dotenv
-
 # Load environment variables from .env file
-load_dotenv()
+# load_dotenv()
 
 class Settings(BaseSettings):
     OPENAI_API_KEY: str
     LANGCHAIN_API_KEY: str
     LANGCHAIN_TRACING_V2: str
     LANGCHAIN_PROJECT: str
+    LANGCHAIN_ENDPOINT: str
 
     class Config:
-        env_file = ".env"  # File .env di root project
+        env_file = ".env"
 
 settings = Settings()
 
